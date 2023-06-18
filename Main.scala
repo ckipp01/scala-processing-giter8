d31c8446e77e2c52e0cbf5eeedc675d0afcd903e
Chris Kipp <ckipp@pm.me>
2023-06-18 13:28:18 +0200

    *

---
+++ Main.scala
@@ -0,0 +1,13 @@
+import processing.core
+import processing.core.*
+import processing.core.PApplet
+
+@main def run(): Unit = PApplet.main("Main")
+
+class Main extends PApplet:
+
+  override def settings(): Unit =
+    size(400, 400)
+
+  override def draw(): Unit =
+    line(150, 25, mouseX.toFloat, mouseY.toFloat)
